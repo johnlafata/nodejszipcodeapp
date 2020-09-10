1f513fc2a398c1dbb9772d78cbdf92b142002d98
John LaFata <jlafata@pivotal.io>
2020-09-10 19:30:00 -0400

    adds tracing and cleans up node_modules

--- api/controller.js
+++ api/controller.js
@@ -2,21 +2,53 @@
 
 var properties = require('../package.json')
 var distance = require('../service/distance');
+/* add for tracing */
+var initTracer = require('jaeger-client').initTracer;
+
+const config = {
+  serviceName: 'my-awesome-zipcode-servicei',
+  sampler: {
+  type: 'const',
+   param: 1,
+  },
+  reporter: {
+    collectorEndpoint: 'http://localhost:30080/api/traces',
+    logSpans: true, // this logs whenever we send a span
+  },
+};
+
+const options = {
+ logger: {
+  info: function logInfo(msg) {
+  console.log('INFO  ', msg);
+  },
+  error: function logError(msg) {
+  console.log('ERROR ', msg);
+  },
+ },
+};
+var tracer = initTracer(config, options);
+
+/* end of add for tracing */
 
 var controllers = {
    about: function(req, res) {
+       const span=tracer.startSpan('about');
        var aboutInfo = {
            name: properties.name,
            version: properties.version
        }
        res.json(aboutInfo);
+       span.finish();
    },
    getDistance: function(req, res) {
+           const span=tracer.startSpan('distance');
            distance.find(req, res, function(err, dist) {
                if (err)
                    res.send(err);
                res.json(dist);
            });
+           span.finish();
        },
 };
 
--- server.js
+++ server.js
@@ -2,27 +2,6 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
-/* add for tracing */
-/*
-var initTracer = require('jaeger-client').initTracer;
-
-// See schema https://github.com/jaegertracing/jaeger-client-node/blob/master/src/configuration.js#L37
-
-var config = {
-  serviceName: 'my-awesome-service',
-};
-var options = {
-  tags: {
-    'my-awesome-service.version': '1.1.2',
-  },
-  metrics: metrics,
-  logger: logger,
-};
-var tracer = initTracer(config, options);
-*/
-/* end of add for tracing */
-
-
 const routes = require('./api/routes');
 routes(app);
 app.listen(port, function() {
